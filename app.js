0ff88f26100dd13c423f606d416105f13d1629fc
Alejandro Blanco <ablanco@yaco.es>
2012-03-12 11:19:23 +0100

    Ver #95 Hago que los ficheros JS pasen jslint

--- app.js
+++ app.js
@@ -1,31 +1,63 @@
+/*jslint vars: false, node: true, nomen: true */
+/*global */
+
+// Copyright 2012 Yaco Sistemas S.L.
+//
+// Developed by Yaco Sistemas <ablanco@yaco.es>
+//
+// Licensed under the EUPL, Version 1.1 or – as soon they
+// will be approved by the European Commission - subsequent
+// versions of the EUPL (the "Licence");
+// You may not use this work except in compliance with the
+// Licence.
+// You may obtain a copy of the Licence at:
+//
+// http://joinup.ec.europa.eu/software/page/eupl
+//
+// Unless required by applicable law or agreed to in
+// writing, software distributed under the Licence is
+// distributed on an "AS IS" basis,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied.
+// See the Licence for the specific language governing
+// permissions and limitations under the Licence.
 
 /**
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-
-var app = module.exports = express.createServer();
+var express = require('express'),
+    routes = require('./routes'),
+    app = module.exports = express.createServer();
 
 // Configuration
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'html');
-  app.register('.html', require('jqtpl').express);
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+app.configure(function () {
+    "use strict";
+
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'html');
+    app.register('.html', require('jqtpl').express);
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    // static is not in dot notation because of JSLint
+    app.use(express['static'](__dirname + '/public'));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+app.configure('development', function () {
+    "use strict";
+
+    app.use(express.errorHandler({
+        dumpExceptions: true,
+        showStack: true
+    }));
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
+app.configure('production', function () {
+    "use strict";
+
+    app.use(express.errorHandler());
 });
 
 // Routes
