fc860abcc22ff0ad45d6fbd800115e567c26cfb2
Alejandro Blanco <ablanco@yaco.es>
2012-03-12 11:38:25 +0100

    Ver #95 Fichero settings con el puerto y el root de la app

--- app.js
+++ app.js
@@ -28,6 +28,7 @@
 
 var express = require('express'),
     routes = require('./routes'),
+    settings = require('./settings'),
     app = module.exports = express.createServer();
 
 // Configuration
@@ -62,7 +63,7 @@ app.configure('production', function () {
 
 // Routes
 
-app.get('/', routes.index);
+app.get(settings.global.root + '/', routes.index);
 
-app.listen(3000);
+app.listen(settings.global.port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
