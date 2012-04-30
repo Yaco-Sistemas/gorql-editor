c749b8caf512507777f391a8f6349cb9426b91f7
Alejandro Blanco <ablanco@yaco.es>
2012-04-30 12:23:00 +0200

    Ver #131 Cambios en la estructura de ficheros para que case con lo que pone en la documentación
    --HG--
    rename : gorql-editor.spec => specs/gorql-editor.spec

--- make_dev_rpm.sh
+++ make_dev_rpm.sh
@@ -17,12 +17,12 @@ cd ${CURRENT_DIR}
 
 export LANG=C
 REV=`hg parent | cut -d" " -f4 | cut -d":" -f1 | head -n 1`
-VERSION=`grep "Version: " ${PACKAGE}.spec | cut -d" " -f2 | cut -d"h" -f1`
+VERSION=`grep "Version: " specs/${PACKAGE}.spec | cut -d" " -f2 | cut -d"h" -f1`
 TAG="${VERSION}hg${REV}"
 
 echo "New version: ${TAG}"
 
-sed -i "s/Version: [0-9].[0-9].[0-9]develhg[0-9]\+/Version: ${TAG}/g" ${PACKAGE}.spec
+sed -i "s/Version: [0-9].[0-9].[0-9]develhg[0-9]\+/Version: ${TAG}/g" specs/${PACKAGE}.spec
 
 cd /tmp/${PACKAGE}
 hg archive -t tgz ${CURRENT_DIR}/${PACKAGE}-${TAG}.tar.gz
@@ -37,7 +37,7 @@ if [ -f ${BUILD_ROOT}/SPECS/${PACKAGE}.spec ]
 then
     rm -f ${BUILD_ROOT}/SPECS/${PACKAGE}.spec
 fi
-ln -s ${CURRENT_DIR}/${PACKAGE}.spec ${BUILD_ROOT}/SPECS/
+ln -s ${CURRENT_DIR}/specs/${PACKAGE}.spec ${BUILD_ROOT}/SPECS/
 
 cd ${BUILD_ROOT}/SPECS/
 rm -rf ${BUILD_ROOT}/BUILD/*
